aa3bcf249c344e2c511a633d47be3d1939ec554b
Community85 <timothy.bergenheim@gmail.com>
2020-01-02 10:49:55 -0500

    squadcommands ix to nut

--- simplesquad/sh_squadcommands.lua
+++ simplesquad/sh_squadcommands.lua
@@ -8,11 +8,11 @@
 	DESCRIPTION: Prints all currently populated squads to the console.
 ]]
 
-ix.command.Add("printsquads", {
+nut.command.Add("printsquads", {
 	syntax = "<none>",
 	description = "Prints the squads.",
 	OnRun = function(self, client)
-		for k, v in pairs(ix.squadsystem.squads) do
+		for k, v in pairs(nut.squadsystem.squads) do
 			print(k.." = {")
 			for i, j in pairs(v) do
 				print("     "..i.." = "..util.TypeToString(j.member)..", "..util.TypeToString(j.color))
@@ -27,11 +27,11 @@ ix.command.Add("printsquads", {
 	DESCRIPTION: Prints a player's squad info to the console.
 ]]
 
-ix.command.Add("printsquadinfo", {
+nut.command.Add("printsquadinfo", {
 	syntax = "<player>",
 	description = "prints squad info of target.",
 	arguments = {
-		ix.type.character
+		nut.type.character
 	},
 	OnRun = function(self, client, target)
 		local squadInfo = target:GetData("squadInfo")
@@ -53,11 +53,11 @@ ix.command.Add("printsquadinfo", {
 	DESCRIPTION: Clears the squad info of a player.
 ]]
 
-ix.command.Add("clearsquadinfo", {
+nut.command.Add("clearsquadinfo", {
 	syntax = "<player>",
 	description = "Clears squad info of target.",
 	arguments = {
-		ix.type.character
+		nut.type.character
 	},
 	OnRun = function(self, client, target)
 		target:ClearSquadInfo()
@@ -69,12 +69,12 @@ ix.command.Add("clearsquadinfo", {
 	DESCRIPTION: Resyncs all squads with the server's information.
 ]]
 
-ix.command.Add("syncallsquads", {
+nut.command.Add("syncallsquads", {
 	syntax = "<none>",
 	description = "Resyncs all squads.",
 	OnRun = function(self, client)
-		for k, _ in pairs(ix.squadsystem.squads) do
-			ix.squadsystem.SyncSquad(k)
+		for k, _ in pairs(nut.squadsystem.squads) do
+			nut.squadsystem.SyncSquad(k)
 		end
 	end
 })
